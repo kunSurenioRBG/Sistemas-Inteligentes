883b7d1266b17e0eb67e5a2386307d7531fb133e
kunSurenioRBG <poncerochasanti@hotmail.com>
2023-03-21 08:20:35 +0100

    A*_maze added

---
+++ Practicas/A-_laberinto_2/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        Laberinto l1 = new Laberinto();
+        l1.printConsole();
+        l1.solve();
+        l1.printSalida();
+    }
+}
